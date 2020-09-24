c80961167397291b7a70454a86f35e3a8baf47f6
Ankit Upadhyay <56730118+ankitupadhyay889@users.noreply.github.com>
2020-09-24 16:36:55 +0530

    Updates

---
+++ precache-manifest.2058b70da4a9884d533b1e240b2312cc.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08970ac256a34571d8f9c71ee1184d6d",
+    "url": "/reactwebdev/index.html"
+  },
+  {
+    "revision": "58a40f62c5c87ecbb5c9",
+    "url": "/reactwebdev/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a16a2a3eae29612a7232",
+    "url": "/reactwebdev/static/css/main.f70fe5ee.chunk.css"
+  },
+  {
+    "revision": "58a40f62c5c87ecbb5c9",
+    "url": "/reactwebdev/static/js/2.1775a3ee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactwebdev/static/js/2.1775a3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16a2a3eae29612a7232",
+    "url": "/reactwebdev/static/js/main.90688f81.chunk.js"
+  },
+  {
+    "revision": "89f6be3cc80ea561b221",
+    "url": "/reactwebdev/static/js/runtime-main.313530ce.js"
+  },
+  {
+    "revision": "0784180084c6b011da620abf7f275770",
+    "url": "/reactwebdev/static/media/ada.07841800.jpg"
+  },
+  {
+    "revision": "b6b4732182fb5a7a3e20cb0e53618faf",
+    "url": "/reactwebdev/static/media/ade.b6b47321.jpg"
+  },
+  {
+    "revision": "a3b873987c2c66236099243901685982",
+    "url": "/reactwebdev/static/media/ase.a3b87398.jpg"
+  },
+  {
+    "revision": "ef79ccb0eed48ddb6c41040139846627",
+    "url": "/reactwebdev/static/media/aw.ef79ccb0.jpg"
+  },
+  {
+    "revision": "826444c21e531a749e2a2acef43246d7",
+    "url": "/reactwebdev/static/media/awee.826444c2.png"
+  },
+  {
+    "revision": "5fe2a6cf0628e22d6d2e0b06d537c9cb",
+    "url": "/reactwebdev/static/media/cnwi.5fe2a6cf.jpg"
+  },
+  {
+    "revision": "7d71c798ded07b71d27d6f4c5046ab50",
+    "url": "/reactwebdev/static/media/cre.7d71c798.jpg"
+  },
+  {
+    "revision": "ab9ac90f123ae9eb98c8762e89276e95",
+    "url": "/reactwebdev/static/media/crea.ab9ac90f.jpg"
+  },
+  {
+    "revision": "e3bb83f7de3e2ebc7f74caab4ab9272c",
+    "url": "/reactwebdev/static/media/hja.e3bb83f7.png"
+  },
+  {
+    "revision": "94072ad3189029b573738b02992ff74b",
+    "url": "/reactwebdev/static/media/lak.94072ad3.jpg"
+  },
+  {
+    "revision": "34480372048f4064c1193b39213a4764",
+    "url": "/reactwebdev/static/media/lkal.34480372.jpg"
+  },
+  {
+    "revision": "22637e3cd2d02a1f9f5d02ab48d4d28d",
+    "url": "/reactwebdev/static/media/lp.22637e3c.jpg"
+  },
+  {
+    "revision": "a1ffedb89f4035d822623ecf0f558786",
+    "url": "/reactwebdev/static/media/lpaq.a1ffedb8.png"
+  },
+  {
+    "revision": "294613a0e9e84edbabce45474263b85b",
+    "url": "/reactwebdev/static/media/poa.294613a0.jpg"
+  },
+  {
+    "revision": "65c0c1dbf1e8854e3b3952e98f8705a0",
+    "url": "/reactwebdev/static/media/qpo.65c0c1db.png"
+  },
+  {
+    "revision": "a04ef0d99a8997d26e9d1f46eff8c190",
+    "url": "/reactwebdev/static/media/qwqq.a04ef0d9.png"
+  }
+]);
